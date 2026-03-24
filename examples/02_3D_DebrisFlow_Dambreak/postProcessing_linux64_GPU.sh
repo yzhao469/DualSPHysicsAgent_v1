7179227c990885aa4a594e3b798afec6c7135025
yzhao469 <79812633+yzhao469@users.noreply.github.com>
2026-03-24 00:32:22 -0500

    Added reference examples for evaluation

---
+++ examples/02_3D_DebrisFlow_Dambreak/postProcessing_linux64_GPU.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+fail () { 
+ echo Execution aborted. 
+ read -n1 -r -p "Press any key to continue..." key 
+ exit 1 
+}
+
+# "name" and "dirout" are named according to the testcase
+
+export name=3D_DebrisFlow_Dambreak
+export dirout=${name}_out
+export diroutdata=${dirout}/data
+
+# "executables" are renamed and called from their directory
+
+export dirbin=../../../bin/linux
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${dirbin}
+export gencase="${dirbin}/GenCase_linux64"
+export dualsphysicscpu="${dirbin}/DualSPHysics5.0_NNewtonianCPU_linux64"
+export dualsphysicsgpu="${dirbin}/DualSPHysics5.0_NNewtonian_linux64"
+export boundaryvtk="${dirbin}/BoundaryVTK_linux64"
+export partvtk="${dirbin}/PartVTK_linux64"
+export partvtkout="${dirbin}/PartVTKOut_linux64"
+export measuretool="${dirbin}/MeasureTool_linux64"
+export computeforces="${dirbin}/ComputeForces_linux64"
+export isosurface="${dirbin}/IsoSurface_linux64"
+export flowtool="${dirbin}/FlowTool_linux64"
+export floatinginfo="${dirbin}/FloatingInfo_linux64"
+
+# Executes PartVTK to create VTK files with particles.
+export dirout2=${dirout}/particles
+${partvtk} -dirin ${diroutdata} -savevtk ${dirout2}/PartFluid -onlytype:-all,fluid -vars:+idp,+vel,+rhop,+press,+mk
+${partvtk} -dirin ${diroutdata} -savevtk ${dirout2}/PartBound -onlytype:-all,bound -vars:+idp,+vel,+rhop,+press,+mk
+if [ $? -ne 0 ] ; then fail; fi
+
+if [ $option != 3 ];then
+ echo All done
+ else
+ echo Execution aborted
+fi
+
+read -n1 -r -p "Press any key to continue..." key
